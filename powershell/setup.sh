e0502f1508f629900e1caa180f6e8db9ec68e290
sky-joker <sky.jokerxx@gmail.com>
2019-09-14 17:43:31 +0900

    modify travis ci file and setup.sh

--- powershell/setup.sh
+++ powershell/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y wget
+apt-get install -y wget libicu55 liblttng-ust0
 wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
 apt-get install -y apt-transport-https
